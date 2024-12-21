8a903243a7533d24d35a5c4a6276666f0c3d4f1f
Nurdaulet <nurdaulet.beketov.2005@gmail.com>
2024-12-22 02:09:20 +0500

    Some changes with data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
+import  {useEffect} from 'react';
+import { Menu } from 'antd';
+import axios from "axios";
+const items = [
 
-function App() {
+            {
+                key: 'g1',
+                label: 'Список криптовалют',
+                type: 'group',
+                children: [
+                    {
+                        key: '1',
+                        label: 'Option 1',
+                    },
+                    {
+                        key: '2',
+                        label: 'Option 2',
+                    },
+                ],
+            },
+];
+const App = () => {
 
-  return (
-    <>
-      hello World
-    </>
-  )
-}
+    const fetchCurrencies = ()=> {
+        axios.get("http://192.168.56.1:8000/cryptocurrencies", {
+        }).then((response) => {
+            console.log(response.data);
+        }).catch((error) => {
+            console.error('fucking error is',error);
+        });
+    }
 
-export default App
+    useEffect(() => {
+        fetchCurrencies();
+    }, []);
+
+    const onClick = (e) => {
+        console.log('click ', e);
+    };
+    return (
+        <Menu
+            onClick={onClick}
+            style={{
+                width: 256,
+            }}
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+            items={items}
+        />
+    );
+};
+export default App;
