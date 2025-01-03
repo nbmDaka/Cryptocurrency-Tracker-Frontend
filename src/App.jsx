94b8c4a7735134a074a34f5846c615597464f3ac
Nurdaulet <nurdaulet.beketov.2005@gmail.com>
2025-01-04 00:23:49 +0500

    Finishing Cryptocurrency projecty

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import {useEffect, useState} from 'react';
-import { Menu } from 'antd';
+import {Menu, Spin} from 'antd';
 import axios from "axios";
 import CryptocurrencyCard from "./components/CryptocurrencyCard.jsx";
-const items = [
 
-            {
-                key: 'g1',
-                label: 'Список криптовалют',
-                type: 'group',
-                children: [
-                    {
-                        key: '1',
-                        label: 'Option 1',
-                    },
-                    {
-                        key: '2',
-                        label: 'Option 2',
-                    },
-                ],
-            },
-];
+
 const App = () => {
 
     const [currencies, setCurrencies] = useState([]);
+    const [currency_id, setCurrencyId] = useState(1);
+    const [currencyData, setCurrencyData] = useState(null);
 
     const fetchCurrencies = ()=> {
         axios.get("http://192.168.56.1:8000/cryptocurrencies", {
@@ -43,14 +29,33 @@ const App = () => {
         }).catch((error) => {
             console.error('fucking error is',error);
         });
-    }
+    };
+
+    const fetchCurrency = ()=> {
+        axios.get(`http://192.168.56.1:8000/cryptocurrencies/${currency_id}`, {
+        }).then((response) => {
+            const data = Object.values(response.data)[0]; // Get the first value in the object
+            console.log("second response", data);
+            setCurrencyData(data);
+        }).catch((error) => {
+            console.error('fucking error is',error);
+        });
+    };
+
+
 
     useEffect(() => {
         fetchCurrencies();
     }, []);
 
+    useEffect(() => {
+        setCurrencyData(null)
+        fetchCurrency();
+    }, [currency_id]);
+
     const onClick = (e) => {
         console.log('click ', e);
+        setCurrencyId(e.key)
     };
     return (
         <div className="flex ">
@@ -66,7 +71,7 @@ const App = () => {
                 className="h-screen overflow-scroll"
             />
             <div className="mx-auto my-auto">
-                <CryptocurrencyCard></CryptocurrencyCard>
+                {currencyData ? <CryptocurrencyCard currency={currencyData}/> : <Spin size='large'/>}
             </div>
         </div>
     );
--- src/components/CryptocurrencyCard.jsx
+++ src/components/CryptocurrencyCard.jsx
@@ -1,16 +1,20 @@
 import {Card} from "antd";
 
-function CryptocurrencyCard() {
+function CryptocurrencyCard(props) {
+
+    const { currency } = props
+    console.log('Fucking currency',currency)
+    const price = currency.quote.USD.price
 
     return (
         <div>
             <Card title={
                 <div className="flex items-center gap-3">
-                    <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"/>
-                    <span>Bitcoin</span>
+                    <img src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${currency.id}.png`}/>
+                    <span>{currency.name}</span>
                 </div>
             } extra={<a href="#">More</a>} style={{ width: 300 }}>
-                <p>Card content</p>
+                <p>Текущая цена: {price}</p>
                 <p>Card content</p>
                 <p>Card content</p>
             </Card>
