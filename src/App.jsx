a95242451608d7fe9e2385ea67812ae79acd35d2
Nurdaulet <nurdaulet.beketov.2005@gmail.com>
2025-01-03 23:39:24 +0500

    Showing in frontend backend fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import  {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import { Menu } from 'antd';
 import axios from "axios";
+import CryptocurrencyCard from "./components/CryptocurrencyCard.jsx";
 const items = [
 
             {
@@ -21,10 +22,24 @@ const items = [
 ];
 const App = () => {
 
+    const [currencies, setCurrencies] = useState([]);
+
     const fetchCurrencies = ()=> {
         axios.get("http://192.168.56.1:8000/cryptocurrencies", {
         }).then((response) => {
-            console.log(response.data);
+            console.log("response", response);
+            const currenciesResponse = response.data;
+            const menuItems = [
+                {
+                    key: 'g1',
+                    label: 'Список криптовалют',
+                    type: 'group',
+                    children: currenciesResponse.map((item) => {
+                        return {label: item.name, key: item.id}
+                    })
+                },
+            ]
+            setCurrencies(menuItems);
         }).catch((error) => {
             console.error('fucking error is',error);
         });
@@ -38,16 +53,22 @@ const App = () => {
         console.log('click ', e);
     };
     return (
-        <Menu
-            onClick={onClick}
-            style={{
-                width: 256,
-            }}
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            mode="inline"
-            items={items}
-        />
+        <div className="flex ">
+            <Menu
+                onClick={onClick}
+                style={{
+                    width: 256,
+                }}
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+                items={currencies}
+                className="h-screen overflow-scroll"
+            />
+            <div className="mx-auto my-auto">
+                <CryptocurrencyCard></CryptocurrencyCard>
+            </div>
+        </div>
     );
 };
 export default App;
